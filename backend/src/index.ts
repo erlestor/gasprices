fb0d116b99bf4acc7d3062cefc4de3a782a73d0c
sondresorbye0111 <sondre@magson.no>
2022-10-14 12:21:36 +0200

    #1 graphql get gas prices per gas station

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,58 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
+import { GasPrice, GasStation, Resolvers } from "./generated/graphql";
 
-const testPosts = [
+const testPosts: GasStation[] = [
   {
-    id: 1,
-    title: "Used car",
-    description: "I am selling my used car",
-    price: 1000,
+    id: "1",
+    name: "Nardo Esso",
+    city: "Trondheim",
   },
   {
-    id: 2,
-    title: "Used bike",
-    description: "I am selling my used bike",
-    price: 500,
+    id: "2",
+    name: "Nardo Shell",
+    city: "Trondheim",
   },
   {
-    id: 3,
-    title: "Used scooter",
-    description: "I am selling my used scooter",
-    price: 200,
+    id: "3",
+    name: "Nardo Circle K",
+    city: "Trondheim",
   },
 ];
 
-const resolvers = {
-  Query: {
-    getAllPosts: (_, args) => {
-      let { maxPrice } = args;
-      maxPrice = maxPrice || 1000000;
-      return testPosts.filter((post) => post.price <= maxPrice);
+const testPrices: {[key: string]: GasPrice[]} = {
+  "1": [
+    {
+      id: "1",
+      price: 12.5,
+      createdAt: new Date().getTime(),
     },
+    {
+      id: "2",
+      price: 10.1,
+      createdAt: new Date().getTime(),
+    },
+  ],
+};
+
+const resolvers: Resolvers = {
+  Query: {
+    gasStations: (_, args) => {
+      return testPosts;
+    },
+  },
+  GasStation: {
+    prices: (parent, args) => {
+      return testPrices[parent.id];
+    },
+    latestPrice: (parent, args) => {
+      return testPrices[parent.id][0];
+    }
   },
 };
 
-const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
+const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
 const server = new ApolloServer({
   typeDefs,
   resolvers,
