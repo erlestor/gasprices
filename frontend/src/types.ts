9f7a4484dd7acd78a88beb3bf0dc9e7d2d50e75f
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-10-15 13:20:04 +0200

    #9 Types for the client

---
+++ frontend/src/types.ts
@@ -0,0 +1,15 @@
+
+export interface GasPrice {
+    id: string;
+    price: number;
+    createdAt: string;
+}
+
+export interface GasStation {
+    id: string;
+    name: string;
+    city: string;
+    latestPrice?: GasPrice;
+    prices?: GasPrice[];
+}
+
