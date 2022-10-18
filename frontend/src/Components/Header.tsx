33592171dc31054b3332ceca58a83d96509a9db7
Magnus Løvhøiden <malovhoi@stud.ntnu.no>
2022-10-18 12:40:47 +0200

    #5 removed dependencies

--- frontend/src/Components/Header.tsx
+++ frontend/src/Components/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './header.module.css';
 import { Link } from 'react-router-dom';
-import { BsSearch } from "react-icons/bs";
 
 
 const headernameLink = {
