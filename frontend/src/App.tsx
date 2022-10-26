a38fd29a5771046cbefa0763e660fd878bc13d83
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 02:29:19 +0200

    update imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import "./App.css";
-import Frontpage from "./Components/Frontpage";
+import React from "react"
+import "./App.css"
+import Frontpage from "./Components/frontpage/Frontpage"
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   HttpLink,
   from,
-} from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddItemPage from "./Components/AddItemPage";
+} from "@apollo/client"
+import { onError } from "@apollo/client/link/error"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AddItemPage from "./Components/addItem/AddItemPage"
 
 //Error handling from the Apollo docs: Advanced Error Handling
 //https://www.apollographql.com/docs/react/data/error-handling/
@@ -20,22 +20,22 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
       console.log(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
-    );
+    )
   if (networkError) {
-    console.log(`[Network error]: ${networkError}`);
+    console.log(`[Network error]: ${networkError}`)
   }
-});
+})
 
 const link = from([
   errorLink,
   new HttpLink({ uri: "http://localhost:4000/graphql" }),
-]);
+])
 
 //The apollo client
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: link,
-});
+})
 
 function App() {
   return (
@@ -47,7 +47,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
-  );
+  )
 }
 
-export default App;
+export default App
