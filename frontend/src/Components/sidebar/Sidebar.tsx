8b854d034895bd971a60b37c4e7a3654e2cb1b99
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 02:28:33 +0200

    adapt filter to gas stations

--- frontend/src/Components/sidebar/Sidebar.tsx
+++ frontend/src/Components/sidebar/Sidebar.tsx
@@ -1,28 +1,59 @@
-import React, { useState } from "react";
-import styles from "./sidebar.module.css";
-import { BsFilterLeft } from "react-icons/bs";
-import { BsCaretLeftFill } from "react-icons/bs";
+import React, { useEffect, useState } from "react"
+import styles from "./sidebar.module.css"
+import { BsFilterLeft } from "react-icons/bs"
+import { BsCaretLeftFill } from "react-icons/bs"
 
 type SidebarProps = {
-  collapsed: boolean;
-};
+  collapsed: boolean
+  priceSliderValue: number
+  setPriceSliderValue: (val: number) => void
+  cities: string[]
+  setCities: (cities: string[]) => void
+}
 
-export default function SideBar({ collapsed }: SidebarProps) {
-  const [priceSliderValue, setPriceSliderValue] = useState(400);
-  const [menuCollapse, setMenuCollapse] = useState(collapsed);
+export default function SideBar({
+  collapsed,
+  priceSliderValue,
+  setPriceSliderValue,
+  cities,
+  setCities,
+}: SidebarProps) {
+  const [menuCollapse, setMenuCollapse] = useState(collapsed)
+
+  useEffect(() => {
+    console.log(cities)
+  }, [cities])
 
   const handlePriceSliderChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
-    setPriceSliderValue(event.target.valueAsNumber);
-  };
+    setPriceSliderValue(event.target.valueAsNumber)
+  }
 
   function closeMenu() {
-    setMenuCollapse(true);
+    setMenuCollapse(true)
   }
 
   function showMenu() {
-    setMenuCollapse(false);
+    setMenuCollapse(false)
+  }
+
+  const handleCityChange = (e: any) => {
+    const checked = e.target.checked
+    const city = e.target.name
+
+    if (checked) {
+      setCities([...cities, city])
+      return
+    }
+
+    const newCities = cities.filter((c) => c !== city)
+    setCities(newCities)
+  }
+
+  const clearFilter = () => {
+    setCities([])
+    setPriceSliderValue(25)
   }
 
   return (
@@ -43,46 +74,56 @@ export default function SideBar({ collapsed }: SidebarProps) {
           </div>
           <div className={styles.sideBarSubHeader}>
             <h5>Filter by</h5>
-            <button>Clear</button>
+            <button onClick={clearFilter}>Clear</button>
           </div>
           <div className={styles.sideBarMain}>
             <div className={styles.sideBarCategoryCheckBox}>
-              <h5>Område</h5>
+              <h5>By</h5>
               <div className={styles.sideBarCategoryChoiceCheckBox}>
-                <input type="checkbox" id="agder" name="agder" />
-                <label htmlFor="agder">Agder</label>
-                <span>320</span>
-              </div>
-              <div className={styles.sideBarCategoryChoiceCheckBox}>
-                <input type="checkbox" id="oslo" name="oslo" />
-                <label htmlFor="oslo">Oslo</label>
+                <input
+                  type="checkbox"
+                  id="Oslo"
+                  name="Oslo"
+                  onChange={handleCityChange}
+                />
+                <label htmlFor="Oslo">Oslo</label>
                 <span>1502</span>
               </div>
               <div className={styles.sideBarCategoryChoiceCheckBox}>
-                <input type="checkbox" id="trondelag" name="trondelag" />
-                <label htmlFor="trondelag">Trøndelag</label>
+                <input
+                  type="checkbox"
+                  id="Trondheim"
+                  name="Trondheim"
+                  onChange={handleCityChange}
+                />
+                <label htmlFor="Trondheim">Trondheim</label>
                 <span>301</span>
               </div>
               <div className={styles.sideBarCategoryChoiceCheckBox}>
-                <input type="checkbox" id="viken" name="viken" />
-                <label htmlFor="viken">Viken</label>
+                <input
+                  type="checkbox"
+                  id="test"
+                  name="test"
+                  onChange={handleCityChange}
+                />
+                <label htmlFor="test">Test</label>
                 <span>152</span>
               </div>
             </div>
             <div className={styles.sideBarCategoryRange}>
-              <h5>Pris</h5>
+              <h5>Maks pris</h5>
               <span>{priceSliderValue} kr</span>
               <input
                 type="range"
                 id="price"
                 name="price"
                 min="0"
-                max="1000"
+                max="100"
                 value={priceSliderValue}
                 onChange={handlePriceSliderChange}
               />
             </div>
-            <div className={styles.sideBarCategoryCheckBox}>
+            {/* <div className={styles.sideBarCategoryCheckBox}>
               <h5>Kjøp eller leie</h5>
               <div className={styles.sideBarCategoryChoiceCheckBox}>
                 <input type="checkbox" id="kjope" name="kjope" />
@@ -94,10 +135,10 @@ export default function SideBar({ collapsed }: SidebarProps) {
                 <label htmlFor="leie">Leie</label>
                 <span>15</span>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       )}
     </div>
-  );
+  )
 }
