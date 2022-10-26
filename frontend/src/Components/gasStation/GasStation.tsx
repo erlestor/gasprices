9cd45399d40de3012c8e105bac57ff78956792f6
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 02:48:48 +0200

    add fuelpump icon

--- frontend/src/Components/gasStation/GasStation.tsx
+++ frontend/src/Components/gasStation/GasStation.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react"
-import { GasStation } from "../../types"
 import styles from "./gasStation.module.css"
+import { GasStation } from "../../types"
+import { FaGasPump } from "react-icons/fa"
 
 interface Props {
   station: GasStation
@@ -9,14 +10,17 @@ interface Props {
 const GasStationC: FC<Props> = ({ station }) => {
   return (
     <div className={styles.stationCard}>
-      <h2>
-        {station.name} - {station.city}
+      <h2 className={styles.stationCardHeader}>
+        {station.name}, {station.city}
       </h2>
-      <h3>
-        {station.latestPrice
-          ? station.latestPrice.toFixed(2) + " kr/l"
-          : "Ingen nylige priser"}
-      </h3>
+      <div className={styles.fuelContainer}>
+        <FaGasPump className={styles.pumpIcon} />
+        <h3>
+          {station.latestPrice
+            ? station.latestPrice.toFixed(2) + " kr/l"
+            : "Ingen nylige priser"}
+        </h3>
+      </div>
     </div>
   )
 }
