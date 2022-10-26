10b8ffa8f8521c8b5b32f80d7d0e45fb0d9adf5a
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 22:06:57 +0200

    stationPage links to new page

--- frontend/src/Components/gasStation/GasStation.tsx
+++ frontend/src/Components/gasStation/GasStation.tsx
@@ -1,28 +1,29 @@
-import { FC } from "react";
-import styles from "./gasStation.module.css";
-import { FaGasPump } from "react-icons/fa";
-import { GasStation } from "../../graphql/types";
+import { FC } from "react"
+import styles from "./gasStation.module.css"
+import { FaGasPump } from "react-icons/fa"
+import { GasStation } from "../../graphql/types"
+import { Link } from "react-router-dom"
 
 interface Props {
-  station: GasStation;
+  station: GasStation
 }
 
 const GasStationC: FC<Props> = ({ station }) => {
   return (
-    <div className={styles.stationCard}>
-      <h2 className={styles.stationCardHeader}>
-        {station.name}, {station.city}
-      </h2>
-      <div className={styles.fuelContainer}>
-        <FaGasPump className={styles.pumpIcon} />
-        <h3>
-          {station.latestPrice
-            ? station.latestPrice.toFixed(2) + " kr/l"
-            : "Ingen nylige priser"}
-        </h3>
+    <Link to={`/station/${station.id}`} className={styles.cardLink}>
+      <div className={styles.stationCard}>
+        <h2 className={styles.stationCardHeader}>
+          {station.name}, {station.city}
+        </h2>
+        <div className={styles.fuelContainer}>
+          <FaGasPump className={styles.pumpIcon} />
+          <h3>
+            {station.latestPrice ? station.latestPrice.toFixed(2) + " kr/l" : "Ingen nylige priser"}
+          </h3>
+        </div>
       </div>
-    </div>
-  );
-};
+    </Link>
+  )
+}
 
-export default GasStationC;
+export default GasStationC
