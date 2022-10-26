c1d2ed017a257b17b6bff476cc602f6f8b30f1d0
Magnus Løvhøiden <malovhoi@stud.ntnu.no>
2022-10-26 09:53:19 +0200

    Create cards and update design

--- frontend/src/Components/MainContent.tsx
+++ frontend/src/Components/MainContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./maincontent.module.css";
 import { BsSearch } from "react-icons/bs";
+import circleK  from '../Images/circleK.png';
 
 export default function MainContent() {
   return (
@@ -9,6 +10,64 @@ export default function MainContent() {
         <input type="text" placeholder="Search..."></input>
         <BsSearch className={styles.searchIcon} />
       </div>
+        <div className={styles.mainContent}>
+          <div className={styles.cardStyle}>
+            <div className={styles.imageDiv}>
+              <img className={styles.cardStyleImage} src={circleK} alt="CirkleK logo" />
+            </div>
+            <div className={styles.cardInformation}>
+              <div className={styles.cardAreaDiv}>
+                <span className={styles.cardBrand}>Cirkle K</span>
+                <span className={styles.cardArea}>Nidarvoll</span>
+              </div>
+              <div className={styles.cardPriceDiv}>
+                <span className={styles.cardPrice}>18.87 kr</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardStyle}>
+            <div className={styles.imageDiv}>
+              <img className={styles.cardStyleImage} src={circleK} alt="CirkleK logo" />
+            </div>
+            <div className={styles.cardInformation}>
+              <div className={styles.cardAreaDiv}>
+                <span className={styles.cardBrand}>Cirkle K</span>
+                <span className={styles.cardArea}>Nidarvoll</span>
+              </div>
+              <div className={styles.cardPriceDiv}>
+                <span className={styles.cardPrice}>18.87 kr</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardStyle}>
+            <div className={styles.imageDiv}>
+              <img className={styles.cardStyleImage} src={circleK} alt="CirkleK logo" />
+            </div>
+            <div className={styles.cardInformation}>
+              <div className={styles.cardAreaDiv}>
+                <span className={styles.cardBrand}>Cirkle K</span>
+                <span className={styles.cardArea}>Nidarvoll</span>
+              </div>
+              <div className={styles.cardPriceDiv}>
+                <span className={styles.cardPrice}>18.87 kr</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.cardStyle}>
+            <div className={styles.imageDiv}>
+              <img className={styles.cardStyleImage} src={circleK} alt="CirkleK logo" />
+            </div>
+            <div className={styles.cardInformation}>
+              <div className={styles.cardAreaDiv}>
+                <span className={styles.cardBrand}>Cirkle K</span>
+                <span className={styles.cardArea}>Nidarvoll</span>
+              </div>
+              <div className={styles.cardPriceDiv}>
+                <span className={styles.cardPrice}>18.87 kr</span>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
