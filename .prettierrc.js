560735f9fdde3370615d8db7b029342583f4740d
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 17:14:27 +0200

    add prettierconfig

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'css',
+}
