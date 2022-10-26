1ea69365e096a24213822d2f1fd914f512f5c837
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 22:06:10 +0200

    add query for gastation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,43 +1,52 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { readFileSync } from "fs";
-import mongoose from "mongoose";
-import { GasStationModel, GasPriceModel } from "./dbService";
+import { ApolloServer } from "@apollo/server"
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { readFileSync } from "fs"
+import mongoose from "mongoose"
+import { GasStationModel, GasPriceModel } from "./dbService"
 
 const resolvers = {
   Query: {
     gasStations: async (_, args) => {
-      const { maxPrice, minPrice, city, limit, sortBy, nameSearch, skip } = args;
+      const { maxPrice, minPrice, city, limit, sortBy, nameSearch, skip } = args
       const priceQuery = {
         ...(maxPrice && { $lte: maxPrice }),
         ...(minPrice && { $gte: minPrice }),
-      };
+      }
       const query = {
         ...(city && { city }),
         ...(Object.keys(priceQuery).length && { latestPrice: priceQuery }),
         ...(nameSearch && { name: { $regex: nameSearch, $options: "i" } }),
-      };
+      }
       return GasStationModel.find(query)
         .sort({ [sortBy]: "asc" })
         .skip(skip)
-        .limit(limit) as any;
+        .limit(limit) as any
+    },
+    gasStation: async (_, args) => {
+      const { id } = args
+      const query = {
+        ...(id && { id_: id }),
+      }
+      console.log(query)
+      console.log(GasStationModel.find(query))
+      return GasStationModel.find(query) as any
     },
   },
   GasStation: {
     prices: async (parent, args) => {
-      return GasPriceModel.find({ gasStation: parent.id }) as any;
+      return GasPriceModel.find({ gasStation: parent.id }) as any
     },
   },
   Mutation: {
     createGasStation: async (_, args) => {
-      const gasStation = new GasStationModel(args);
-      return gasStation.save() as any;
+      const gasStation = new GasStationModel(args)
+      return gasStation.save() as any
     },
     createGasPrice: async (_, args) => {
-      const { gasStation } = args;
+      const { gasStation } = args
       // TODO: make it a transaction
-      const gasPrice = new GasPriceModel(args);
-      await gasPrice.save();
+      const gasPrice = new GasPriceModel(args)
+      await gasPrice.save()
       // update latest price on GasStation
       return GasStationModel.updateOne(
         {
@@ -46,26 +55,26 @@ const resolvers = {
         {
           latestPrice: gasPrice.price,
         }
-      );
+      )
     },
   },
-};
+}
 
-const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" });
+const typeDefs = readFileSync("./schema.graphql", { encoding: "utf-8" })
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-});
+})
 
 async function startServer() {
-  const connectionString = "mongodb://admin:admin@it2810-41.idi.ntnu.no:27017/";
-  await mongoose.connect(connectionString);
+  const connectionString = "mongodb://admin:admin@it2810-41.idi.ntnu.no:27017/"
+  await mongoose.connect(connectionString)
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
-  });
+  })
 
-  console.log(`🚀 Server ready at: ${url}`);
+  console.log(`🚀 Server ready at: ${url}`)
 }
 
-startServer();
+startServer()
