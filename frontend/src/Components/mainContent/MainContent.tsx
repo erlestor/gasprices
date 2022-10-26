76f941c6f67171cfcd05e04782e833b71748b768
Erlend Storsve <erlestor@ntnu.no>
2022-10-26 02:29:05 +0200

    add debounce search

---
+++ frontend/src/Components/mainContent/MainContent.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import styles from "./maincontent.module.css"
+import { BsSearch } from "react-icons/bs"
+import { useQuery } from "@apollo/client"
+import { GET_GAS_STATIONS } from "../../graphql/queries"
+import { GasStation } from "../../types"
+import GasStationC from "../gasStation/GasStation"
+
+type Props = {
+  cities: string[]
+  priceSliderValue: number
+}
+
+export default function MainContent({ cities, priceSliderValue }: Props) {
+  const [searchText, setSearchText] = useState("")
+  const [debounceText, setDebounceText] = useState("")
+
+  const { error, loading, data } = useQuery(GET_GAS_STATIONS, {
+    variables: {
+      city: cities[0],
+      maxPrice: priceSliderValue,
+      nameSearch: debounceText,
+    },
+  })
+
+  const debounce = (cb: any, delay = 1000) => {
+    let timeout: NodeJS.Timeout
+
+    return (...args: any) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        cb(...args)
+      }, delay)
+    }
+  }
+
+  const updateDebounceText = debounce((text: string) => {
+    setDebounceText(text)
+  }, 500)
+
+  const handleSearchChange = (e: any) => {
+    setSearchText(e.target.value)
+    updateDebounceText(e.target.value)
+  }
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.searchDiv}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchText}
+          onChange={handleSearchChange}
+        ></input>
+        <div>
+          <BsSearch className={styles.searchIcon} />
+        </div>
+      </div>
+      <div className={styles.stationsContainer}>
+        {data &&
+          data.gasStations.map((station: GasStation, stationIdx: number) => (
+            <GasStationC key={stationIdx} station={station} />
+          ))}
+      </div>
+    </div>
+  )
+}
