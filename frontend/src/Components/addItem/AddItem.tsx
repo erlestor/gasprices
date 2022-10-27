e91290afaf771bbbd5b54c6e2be0e070b2a14fad
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-10-27 20:28:04 +0200

    Formating

--- frontend/src/Components/addItem/AddItem.tsx
+++ frontend/src/Components/addItem/AddItem.tsx
@@ -1,45 +1,39 @@
-import { useMutation } from "@apollo/client";
-import React from "react";
-import { CREATE_GAS_PRICE } from "../../graphql/mutations.graphql";
-import styles from "./additem.module.css";
+import { useMutation } from "@apollo/client"
+import React from "react"
+import { CREATE_GAS_PRICE } from "../../graphql/mutations.graphql"
+import styles from "./additem.module.css"
 
-export default function AddItem() {
-  const [price, setPrice] = React.useState<number | null>(null);
+export default function AddItem(stationName: any, id: any) {
+  const [price, setPrice] = React.useState<number | null>(null)
 
-  //Temporary!!:
-  const [gasStationId, setGasStationId] = React.useState(
-    "634fb2df84b8735fd91bab08"
-  );
-
-  const [createGasPrice, { data, loading, error }] =
-    useMutation(CREATE_GAS_PRICE);
+  const [createGasPrice, { data, loading, error }] = useMutation(CREATE_GAS_PRICE)
 
   const addGasPrice = () => {
     createGasPrice({
       variables: {
-        gasStation: gasStationId,
+        gasStation: id.id,
         price: price,
       },
-    });
+    })
     if (error) {
-      console.log(error);
+      console.log(error)
     }
     if (loading) {
-      return <p>Loading...</p>;
+      return <p>Loading...</p>
     }
-  };
+  }
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.elementForm}>
         <div className={styles.formHeaderText}>
-          <h3>Add new price to "Add prop"</h3>
+          <h3>Add new price to {stationName.stationName}</h3>
         </div>
 
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            addGasPrice();
+          onSubmit={e => {
+            e.preventDefault()
+            addGasPrice()
           }}
         >
           <label htmlFor="price">Pris (kr)</label>
@@ -48,11 +42,11 @@ export default function AddItem() {
             name="price"
             type="number"
             value={price ?? ""}
-            onChange={(event) => setPrice(parseFloat(event.target.value))}
+            onChange={event => setPrice(parseFloat(event.target.value))}
           ></input>
           <input className={styles.submit} type="submit" value="Submit"></input>
         </form>
       </div>
     </div>
-  );
+  )
 }
