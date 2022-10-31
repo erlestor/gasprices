e3de4e11bc852f3c615a923c7f04e513eb3198dc
sondresorbye0111 <sondre@magson.no>
2022-10-31 21:27:11 +0100

    #6 show elements on page

--- frontend/src/__test__/add-price.test.tsx
+++ frontend/src/__test__/add-price.test.tsx
@@ -4,11 +4,12 @@ import { render, screen } from '@testing-library/react';
 import { MockedProvider } from "@apollo/client/testing";
 
 import {GasStationPage} from "../Components/gasStationPage/GasStationPage"
+import AddItem from '../Components/addItem/AddItem';
 
 it("should render without error", () => {
     render (
         <MockedProvider mocks={[]}>
-            <GasStationPage />
+            <AddItem id='1' refetch={null as any} />
         </MockedProvider>
     );
 });
