bc368c992ccec17791305dc9a14b3fb3faaa3db0
Erlend Storsve <erlestor@stud.ntnu.no>
2022-11-01 20:09:25 +0100

    add test-ids

--- frontend/src/Components/mainContent/MainContent.tsx
+++ frontend/src/Components/mainContent/MainContent.tsx
@@ -1,24 +1,24 @@
-import { useQuery, useReactiveVar } from "@apollo/client";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import circleK from "../../Images/circleK.png";
-import esso from "../../Images/esso.png";
-import shell from "../../Images/shell.jpg";
-import unoX from "../../Images/uno-x.png";
-import Filter from "./filterEl";
-import styles from "./maincontent.module.css";
+import { useQuery, useReactiveVar } from "@apollo/client"
+import { AiOutlineLoading3Quarters } from "react-icons/ai"
+import circleK from "../../Images/circleK.png"
+import esso from "../../Images/esso.png"
+import shell from "../../Images/shell.jpg"
+import unoX from "../../Images/uno-x.png"
+import Filter from "./filterEl"
+import styles from "./maincontent.module.css"
 
-import InfiniteScroll from "react-infinite-scroll-component";
-import { Link } from "react-router-dom";
-import { GET_GAS_STATIONS } from "../../graphql/queries.graphql";
-import { hasMoreVar, limit } from "../../state/endlessScrollState";
-import { filterStateVar } from "../../state/filterState";
-import { GasStation, GetGasStationsData } from "../../types";
-import { SearchInputEl } from "./searchInputEl";
-import { useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component"
+import { Link } from "react-router-dom"
+import { GET_GAS_STATIONS } from "../../graphql/queries.graphql"
+import { hasMoreVar, limit } from "../../state/endlessScrollState"
+import { filterStateVar } from "../../state/filterState"
+import { GasStation, GetGasStationsData } from "../../types"
+import { SearchInputEl } from "./searchInputEl"
+import { useEffect } from "react"
 
 export default function MainContent() {
-  const filterState = useReactiveVar(filterStateVar);
-  const hasMore = useReactiveVar(hasMoreVar);
+  const filterState = useReactiveVar(filterStateVar)
+  const hasMore = useReactiveVar(hasMoreVar)
 
   const { error, loading, data, fetchMore } = useQuery<GetGasStationsData>(
     GET_GAS_STATIONS,
@@ -32,26 +32,26 @@ export default function MainContent() {
         limit,
       },
     }
-  );
+  )
 
   useEffect(() => {
-    hasMoreVar(true);
-  }, [filterState]);
+    hasMoreVar(true)
+  }, [filterState])
 
   function loadMoreData() {
     fetchMore({
       variables: {
         skip: data?.gasStations.length,
       },
-    });
+    })
   }
 
   if (error) {
-    return <div>Error: {error.message}</div>;
+    return <div>Error: {error.message}</div>
   }
 
   if (loading) {
-    return <div id="#mainContentLoading">Loading...</div>;
+    return <div id="#mainContentLoading">Loading...</div>
   }
 
   return (
@@ -76,14 +76,14 @@ export default function MainContent() {
         <AiOutlineLoading3Quarters size={20} />
       )}
     </main>
-  );
+  )
 }
 
 function formatPrice(number: number | undefined): string {
   if (!number) {
-    return "Ingen pris";
+    return "Ingen pris"
   }
-  return number.toFixed(2) + "kr";
+  return number.toFixed(2) + "kr"
 }
 
 function gasStationEl(gasStation: GasStation) {
@@ -105,7 +105,9 @@ function gasStationEl(gasStation: GasStation) {
         </div>
         <div className={styles.cardInformation}>
           <div className={styles.cardAreaDiv}>
-            <span className={styles.cardBrand}>{gasStation.name}</span>
+            <span className={styles.cardBrand} data-testid="gasStationName">
+              {gasStation.name}
+            </span>
             <span className={styles.cardArea}>{gasStation.city}</span>
           </div>
           <div className={styles.cardPriceDiv}>
@@ -116,18 +118,18 @@ function gasStationEl(gasStation: GasStation) {
         </div>
       </div>
     </Link>
-  );
+  )
 }
 
 function findImage(brandName: string): string | undefined {
   if (brandName === "Esso") {
-    return esso;
+    return esso
   } else if (brandName === "Shell") {
-    return shell;
+    return shell
   } else if (brandName === "Circle K") {
-    return circleK;
+    return circleK
   } else if (brandName === "Uno-X") {
-    return unoX;
+    return unoX
   }
-  return shell;
+  return shell
 }
--- frontend/src/Components/mainContent/searchInputEl.tsx
+++ frontend/src/Components/mainContent/searchInputEl.tsx
@@ -1,19 +1,19 @@
-import { BsSearch } from "react-icons/bs";
-import { filterStateVar } from "../../state/filterState";
-import { debounce } from "../../service/debounce";
-import styles from "./searchInputEl.module.css";
+import { BsSearch } from "react-icons/bs"
+import { filterStateVar } from "../../state/filterState"
+import { debounce } from "../../service/debounce"
+import styles from "./searchInputEl.module.css"
 
 export function SearchInputEl() {
   const updateDebounceText = debounce((text: string) => {
     filterStateVar({
       ...filterStateVar(),
       nameSearch: text,
-    });
-  }, 200);
+    })
+  }, 200)
 
   const handleSearchElChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updateDebounceText(e.target.value);
-  };
+    updateDebounceText(e.target.value)
+  }
 
   return (
     <div className={styles.searchDiv}>
@@ -22,10 +22,11 @@ export function SearchInputEl() {
         type="text"
         placeholder="Search..."
         onChange={handleSearchElChange}
+        data-testid="searchBar"
       ></input>
       <div>
         <BsSearch className={styles.searchIcon} />
       </div>
     </div>
-  );
+  )
 }
