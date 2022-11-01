9dfab227b96794355097b350c2e4dcb18eb02fdf
Erik Tveraaen Torvbråten <eriktto@stud.ntnu.no>
2022-11-01 18:02:35 +0100

    #15 Comment add price

--- frontend/src/Components/addPrice/AddPrice.tsx
+++ frontend/src/Components/addPrice/AddPrice.tsx
@@ -4,17 +4,24 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { CREATE_GAS_PRICE } from "../../graphql/mutations.graphql";
 import styles from "./addprice.module.css";
 
+//Define the types of the props
 type Props = {
   id: string;
   refetch: (variables: { id: string }) => void;
 };
 
+
 export default function AddPrice({ id, refetch }: Props) {
+
+  //The price that is set in the input field
   const [price, setPrice] = React.useState<number | null>(null);
 
+  //The mutation that add a new price to the given gas station
   const [createGasPrice, { data, loading, error }] =
     useMutation(CREATE_GAS_PRICE);
 
+  //The function that is called when the user clicks the submit button
+  //It uses the mutation to add a new price
   const addGasPrice = async () => {
     await createGasPrice({
       variables: {
@@ -22,15 +29,20 @@ export default function AddPrice({ id, refetch }: Props) {
         price: price,
       },
     });
+    //log the error
     if (error) {
       console.log(error);
     }
+    //Loading
     if (loading) {
       return <AiOutlineLoading3Quarters />;
     }
+    //Refetch the data
     refetch({ id: id });
+    //clear the price state
     setPrice(null);
   };
+
 
   return (
     <div className={styles.wrapper}>
